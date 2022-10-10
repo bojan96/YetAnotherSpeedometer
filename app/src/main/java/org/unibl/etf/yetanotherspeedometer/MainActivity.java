af629cb4fb55dde45c195ffcf6851f659d441732
Bojan Malinic <bojan.malinic@outlook.com>
2022-10-10 19:43:59 +0200

    Enable view binding

--- app/src/main/java/org/unibl/etf/yetanotherspeedometer/MainActivity.java
+++ app/src/main/java/org/unibl/etf/yetanotherspeedometer/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.unibl.etf.yetanotherspeedometer.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        var binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
