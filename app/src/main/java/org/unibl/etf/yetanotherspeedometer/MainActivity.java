6d7c410a113d40a7f5a44a701acdd785ac85b1d6
Bojan Malinic <bojan.malinic@outlook.com>
2022-11-21 01:31:53 +0100

    Add toolbar with recordings menu item

--- app/src/main/java/org/unibl/etf/yetanotherspeedometer/MainActivity.java
+++ app/src/main/java/org/unibl/etf/yetanotherspeedometer/MainActivity.java
@@ -14,6 +14,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 
 import org.unibl.etf.yetanotherspeedometer.databinding.ActivityMainBinding;
 import org.unibl.etf.yetanotherspeedometer.location.LocationService;
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         var binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setSupportActionBar(binding.toolbar);
         var viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
         binding.setLifecycleOwner(this);
         binding.setViewModel(viewModel);
@@ -44,6 +46,12 @@ public class MainActivity extends AppCompatActivity {
         createNotificationChannels();
 
         startForegroundService(new Intent(this, LocationService.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 
     private void createNotificationChannels()
