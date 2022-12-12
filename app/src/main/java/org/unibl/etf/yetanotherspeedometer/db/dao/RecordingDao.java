23e509b72d1dd027bf50abe59a82c48dfa907c87
Bojan Malinic <bojan.malinic@outlook.com>
2022-12-12 23:33:09 +0100

    Draw max speed point

--- app/src/main/java/org/unibl/etf/yetanotherspeedometer/db/dao/RecordingDao.java
+++ app/src/main/java/org/unibl/etf/yetanotherspeedometer/db/dao/RecordingDao.java
@@ -7,6 +7,7 @@ import androidx.room.Insert;
 import androidx.room.Query;
 
 import org.unibl.etf.yetanotherspeedometer.db.entity.Recording;
+import org.unibl.etf.yetanotherspeedometer.db.entity.RecordingMaxSpeedPoint;
 import org.unibl.etf.yetanotherspeedometer.db.entity.RecordingPoint;
 
 import java.util.List;
@@ -33,4 +34,9 @@ public interface RecordingDao {
             "WHERE recording.id = :recordingId " +
             "ORDER BY recordingPoint.orderIndex")
     LiveData<Map<Recording, List<RecordingPoint>>> getRecordingWithPoints(int recordingId);
+
+    @Query("SELECT * FROM " +
+            "recording JOIN recordingMaxSpeedPoint ON recording.id = recordingMaxSpeedPoint.recordingId " +
+            "WHERE recording.id = :recordingId")
+    LiveData<Map<Recording, List<RecordingMaxSpeedPoint>>> getRecordingWithMaxSpeedPoint(int recordingId);
 }
--- app/src/main/java/org/unibl/etf/yetanotherspeedometer/routemap/RouteMapActivity.java
+++ app/src/main/java/org/unibl/etf/yetanotherspeedometer/routemap/RouteMapActivity.java
@@ -13,11 +13,13 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import org.unibl.etf.yetanotherspeedometer.R;
 import org.unibl.etf.yetanotherspeedometer.db.AppDatabase;
 import org.unibl.etf.yetanotherspeedometer.db.entity.Recording;
+import org.unibl.etf.yetanotherspeedometer.db.entity.RecordingMaxSpeedPoint;
 import org.unibl.etf.yetanotherspeedometer.db.entity.RecordingPoint;
 
 import java.util.List;
@@ -51,17 +53,23 @@ public class RouteMapActivity extends AppCompatActivity implements OnMapReadyCal
         database.getRecordingDao()
                 .getRecordingWithPoints(recordingId).observe(this, recordingWithPoints ->
                 {
-                    displayRoute(googleMap, recordingWithPoints);
+                    database.getRecordingDao().getRecordingWithMaxSpeedPoint(recordingId).observe(this, recordingWithMaxSpeedPoint ->
+                    {
+                        displayRoute(googleMap, recordingWithPoints, recordingWithMaxSpeedPoint);
+                    });
+
                 });
     }
 
-    private void displayRoute(GoogleMap googleMap, Map<Recording, List<RecordingPoint>> recordingWithPoints)
+    private void displayRoute(GoogleMap googleMap, Map<Recording, List<RecordingPoint>> recordingWithPoints, Map<Recording, List<RecordingMaxSpeedPoint>> recordingWithMaxSpeedPoint)
     {
+
         var points = List.copyOf(recordingWithPoints.values()).get(0);
         var boundsBuilder = LatLngBounds.builder();
         points.forEach(p -> boundsBuilder.include(new LatLng(p.latitude, p.longitude)));
 
         drawRoute(googleMap, points);
+        drawMaxSpeedMarker(googleMap, points, recordingWithMaxSpeedPoint);
         googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(boundsBuilder.build(),
                 (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, getResources().getDisplayMetrics())));
     }
@@ -73,5 +81,17 @@ public class RouteMapActivity extends AppCompatActivity implements OnMapReadyCal
         routePolyline.color(Color.rgb(0, 0, 255));
         routePolyline.addAll(points.stream().map(p -> new LatLng(p.latitude, p.longitude)).collect(Collectors.toList()));
         googleMap.addPolyline(routePolyline);
+    }
+
+    private void drawMaxSpeedMarker(GoogleMap googleMap, List<RecordingPoint> points, Map<Recording, List<RecordingMaxSpeedPoint>> recordingWithMaxSpeedPoint)
+    {
+        if(recordingWithMaxSpeedPoint.size() == 0)
+            return;
+
+        var maxSpeedPointList = List.copyOf(recordingWithMaxSpeedPoint.values()).get(0);
+        var maxSpeedPoint = maxSpeedPointList.get(0);
+        var point = points.get(maxSpeedPoint.maxSpeedPointIndex);
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(point.latitude, point.longitude)).title("Max Speed"));
     }
 }
