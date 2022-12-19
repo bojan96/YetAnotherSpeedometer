cebf49d912d7e0f508ec5e54f4ce83eb59c8f4d6
Bojan Malinic <bojan.malinic@outlook.com>
2022-12-20 00:57:41 +0100

    Draw start and end points, change the way max speed point is drawn

--- app/src/main/java/org/unibl/etf/yetanotherspeedometer/routemap/RouteMapActivity.java
+++ app/src/main/java/org/unibl/etf/yetanotherspeedometer/routemap/RouteMapActivity.java
@@ -2,8 +2,13 @@ package org.unibl.etf.yetanotherspeedometer.routemap;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.TypedValue;
 
@@ -11,6 +16,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -22,6 +30,7 @@ import org.unibl.etf.yetanotherspeedometer.db.AppDatabase;
 import org.unibl.etf.yetanotherspeedometer.db.entity.Recording;
 import org.unibl.etf.yetanotherspeedometer.db.entity.RecordingMaxSpeedPoint;
 import org.unibl.etf.yetanotherspeedometer.db.entity.RecordingPoint;
+import org.unibl.etf.yetanotherspeedometer.util.UnitFormatters;
 
 import java.util.List;
 import java.util.Map;
@@ -37,6 +46,8 @@ public class RouteMapActivity extends AppCompatActivity implements OnMapReadyCal
     public static final String EXTRA_RECORDING_ID = "extra_recording_id";
     @Inject
     public AppDatabase database;
+    @Inject
+    public UnitFormatters formatters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +83,7 @@ public class RouteMapActivity extends AppCompatActivity implements OnMapReadyCal
 
         drawRoute(googleMap, points);
         drawMaxSpeedMarker(googleMap, points, recordingWithMaxSpeedPoint);
+        drawStartEndPoints(googleMap, points);
         googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(boundsBuilder.build(),
                 (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, getResources().getDisplayMetrics())));
     }
@@ -90,10 +102,32 @@ public class RouteMapActivity extends AppCompatActivity implements OnMapReadyCal
         if(recordingWithMaxSpeedPoint.size() == 0)
             return;
 
+        var recording = recordingWithMaxSpeedPoint.keySet().stream().findFirst().get();
         var maxSpeedPointList = List.copyOf(recordingWithMaxSpeedPoint.values()).get(0);
         var maxSpeedPoint = maxSpeedPointList.get(0);
         var point = points.get(maxSpeedPoint.maxSpeedPointIndex);
 
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(point.latitude, point.longitude)).title("Max Speed"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(point.latitude, point.longitude))
+                .title("Max Speed")
+                .snippet(formatters.formatSpeed(recording.maxSpeed))).showInfoWindow();
+    }
+
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
+    private void drawStartEndPoints(GoogleMap googleMap, List<RecordingPoint> points)
+    {
+        if(points.size() == 0)
+            return;
+        googleMap.addMarker(new MarkerOptions().position(
+                new LatLng(points.get(0).latitude, points.get(0).longitude)).icon(BitmapDescriptorFactory.defaultMarker(120)));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(points.get(points.size() - 1).latitude, points.get(points.size() - 1).longitude))
+                .icon(bitmapDescriptorFromVector(this, R.drawable.ic_finish_flag)));
     }
 }
